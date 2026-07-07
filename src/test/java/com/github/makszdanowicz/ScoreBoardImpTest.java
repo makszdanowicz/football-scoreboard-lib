18f4295b8c437f527750e8fcd410096570ee88d2
Maksim Zdanovich <m.zdanovich@andersenlab.com>
2026-07-07 15:51:31 +0200

    test: add summary ordering test based on total score and insertion order

--- src/test/java/com/github/makszdanowicz/ScoreBoardImpTest.java
+++ src/test/java/com/github/makszdanowicz/ScoreBoardImpTest.java
@@ -114,4 +114,50 @@ public class ScoreBoardImpTest {
         assertThrows(IllegalArgumentException.class,
                 () -> scoreBoard.finishMatch(notExistingTeam, notExistingTeam));
     }
+
+    @Test
+    void shouldReturnEmptySummaryWhenNoMatchesInProgress() {
+        // given
+        ScoreBoard scoreBoard = new ScoreBoardImp();
+
+        // when
+        List<Match> summary = scoreBoard.getSummaryOfMatchesInProgress();
+
+        // then
+        assertTrue(summary.isEmpty());
+    }
+
+    @Test
+    void shouldReturnSummaryOrderedByTotalScoreAndInsertionOrder() {
+        // given
+        ScoreBoard scoreBoard = new ScoreBoardImp();
+        MatchId m1 = new MatchId("Mexico", "Canada");
+        MatchId m2 = new MatchId("Spain", "Brazil");
+        MatchId m3 = new MatchId("Germany", "France");
+        MatchId m4 = new MatchId("Uruguay", "Italy");
+        MatchId m5 = new MatchId("Argentina", "Australia");
+
+        scoreBoard.startNewMatch(m1.homeTeam(), m1.guestTeam());
+        scoreBoard.startNewMatch(m2.homeTeam(), m2.guestTeam());
+        scoreBoard.startNewMatch(m3.homeTeam(), m3.guestTeam());
+        scoreBoard.startNewMatch(m4.homeTeam(), m4.guestTeam());
+        scoreBoard.startNewMatch(m5.homeTeam(), m5.guestTeam());
+
+        scoreBoard.updateScore(m1.homeTeam(), m1.guestTeam(), 0, 5);
+        scoreBoard.updateScore(m2.homeTeam(), m2.guestTeam(), 10, 2);
+        scoreBoard.updateScore(m3.homeTeam(), m3.guestTeam(), 2, 2);
+        scoreBoard.updateScore(m4.homeTeam(), m4.guestTeam(), 6, 6);
+        scoreBoard.updateScore(m5.homeTeam(), m5.guestTeam(), 3, 1);
+
+        // when
+        List<Match> summary = scoreBoard.getSummaryOfMatchesInProgress();
+
+        // then
+        assertEquals(5, summary.size());
+        assertEquals(m4, summary.getFirst().id());
+        assertEquals(m2, summary.get(1).id());
+        assertEquals(m1, summary.get(2).id());
+        assertEquals(m5, summary.get(3).id());
+        assertEquals(m3, summary.get(4).id());
+    }
 }
