6aa3935d9ddb9f20cd8050ad2a95066052bec458
Maksim Zdanovich <m.zdanovich@andersenlab.com>
2026-07-07 17:19:15 +0200

    refactor: introduce ReentrantReadWriteLock to make ScoreBoard thread-safe

--- src/main/java/com/github/makszdanowicz/ScoreBoardImp.java
+++ src/main/java/com/github/makszdanowicz/ScoreBoardImp.java
@@ -1,67 +1,95 @@
 package com.github.makszdanowicz;
 
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ScoreBoardImp implements ScoreBoard{
 
     private final Map<MatchId, Match> activeMatches = new HashMap<>();
     private final Set<String> teamsInPlay = new HashSet<>();
     private final List<Match> finishedMatches = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private long sequence = 0;
+
 
     @Override
     public void startNewMatch(String homeTeam, String guestTeam) {
-        // Standardizing names to lowercase for safe comparison
-        String normalizedHomeTeam = homeTeam.toLowerCase();
-        String normalizedGuestTeam = guestTeam.toLowerCase();
+        lock.writeLock().lock();
+        try {
+            // Standardizing names to lowercase for safe comparison
+            String normalizedHomeTeam = homeTeam.toLowerCase();
+            String normalizedGuestTeam = guestTeam.toLowerCase();
 
-        if (teamsInPlay.contains(normalizedGuestTeam) || teamsInPlay.contains(normalizedHomeTeam)) {
-            throw new IllegalStateException("One or both teams are already playing a match");
+            if (teamsInPlay.contains(normalizedGuestTeam) || teamsInPlay.contains(normalizedHomeTeam)) {
+                throw new IllegalStateException("One or both teams are already playing a match");
+            }
+
+            MatchId matchId = new MatchId(homeTeam, guestTeam);
+            Match match = new Match(matchId, sequence++);
+            activeMatches.put(matchId, match);
+            teamsInPlay.add(normalizedHomeTeam);
+            teamsInPlay.add(normalizedGuestTeam);
+        } finally {
+            lock.writeLock().unlock();
         }
-
-        MatchId matchId = new MatchId(homeTeam, guestTeam);
-        Match match = new Match(matchId, sequence++);
-        activeMatches.put(matchId, match);
-        teamsInPlay.add(normalizedHomeTeam);
-        teamsInPlay.add(normalizedGuestTeam);
     }
 
     @Override
     public void updateScore(String homeTeam, String guestTeam, int homeScore, int guestScore) {
-        MatchId matchId = new MatchId(homeTeam, guestTeam);
-        Match currentMatch = activeMatches.get(matchId);
-        if (currentMatch == null) {
-            throw new IllegalArgumentException("Match not found");
+        lock.writeLock().lock();
+        try {
+            MatchId matchId = new MatchId(homeTeam, guestTeam);
+            Match currentMatch = activeMatches.get(matchId);
+            if (currentMatch == null) {
+                throw new IllegalArgumentException("Match not found");
+            }
+            Match updatedMatch = new Match(currentMatch.id(), homeScore, guestScore, currentMatch.insertionOrder());
+            activeMatches.put(matchId, updatedMatch);
+        } finally {
+            lock.writeLock().unlock();
         }
-        Match updatedMatch = new Match(currentMatch.id(), homeScore, guestScore, currentMatch.insertionOrder());
-        activeMatches.put(matchId, updatedMatch);
     }
 
     @Override
     public void finishMatch(String homeTeam, String guestTeam) {
-        MatchId matchId = new MatchId(homeTeam, guestTeam);
-        Match removedMatch = activeMatches.remove(matchId);
-        if (removedMatch != null) {
-            teamsInPlay.remove(homeTeam.toLowerCase());
-            teamsInPlay.remove(guestTeam.toLowerCase());
-            finishedMatches.add(removedMatch);
-        } else {
-            throw new IllegalArgumentException("Match not found");
+        lock.writeLock().lock();
+        try {
+            MatchId matchId = new MatchId(homeTeam, guestTeam);
+            Match removedMatch = activeMatches.remove(matchId);
+            if (removedMatch != null) {
+                teamsInPlay.remove(homeTeam.toLowerCase());
+                teamsInPlay.remove(guestTeam.toLowerCase());
+                finishedMatches.add(removedMatch);
+            } else {
+                throw new IllegalArgumentException("Match not found");
+            }
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 
 
     @Override
     public List<Match> getSummaryOfMatchesInProgress() {
-        return activeMatches.values().stream()
-                .sorted(Comparator.comparingInt(Match::getTotalScore).reversed()
-                        .thenComparing(Match::insertionOrder, Comparator.reverseOrder()))
-                .toList();
-
+        lock.readLock().lock();
+        try {
+            return activeMatches.values().stream()
+                    .sorted(Comparator.comparingInt(Match::getTotalScore).reversed()
+                            .thenComparing(Match::insertionOrder, Comparator.reverseOrder()))
+                    .toList();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public List<Match> getFinishedMatches() {
-        return List.copyOf(finishedMatches);
+        lock.readLock().lock();
+        try {
+            return List.copyOf(finishedMatches);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
